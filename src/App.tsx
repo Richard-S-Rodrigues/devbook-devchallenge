bac0560ee65136e2dff6d70b11fd5c7c1f059536
Richard <richardsouzarodrigues555@gmail.com>
2021-03-12 22:30:19 -0300

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Header from "./components/Header";
+
+import "./global.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import styles from "./index.module.css";
+
+import { BiSearchAlt2 } from "react-icons/bi";
+
+const Header = () => {
+    return (
+        <header className={styles.container}>
+            <div className={styles.logoContainer}>
+                <h1>
+                    Dev<span>Book</span>
+                </h1>
+            </div>
+            <div className={styles.searchBoxContainer}>
+                <input type="text" placeholder="Search for user" />
+                <BiSearchAlt2
+                    style={{
+                        background: "#fff",
+                        fontSize: "22px",
+                        position: "absolute",
+                        right: "2em",
+                        top: "1.5em",
+                    }}
+                />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
